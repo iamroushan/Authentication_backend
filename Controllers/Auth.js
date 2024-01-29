662a70ec5f2856b27610e363cc517811e81cc728
iamroushan <roushanrnd@gmail.com>
2024-01-30 00:57:23 +0530

    adding the controller for the login page

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -1,5 +1,8 @@
 const bcrypt = require("bcrypt")
 const User= require('../models/User')
+const jwt= require("jsonwebtoken")
+const { options } = require("../routes/user")
+require("dotenv").config()
 
 // signup route handler
 exports.signup = async(req,res) =>{
@@ -43,7 +46,76 @@ exports.signup = async(req,res) =>{
         console.error(error)
         return res.status(500).json({
             success: false,
-            message: "User cannot be registered, please try agaibn later"
+            message: "User cannot be registered, please try again later"
         })
     }
 } 
+
+
+// login
+exports.login= async (req,res)=>{
+    try {
+        
+        //data fetch
+        const {email, password}= req.body
+        // validation on email and password
+        if(!email || !password){
+            return res.status(400).json({
+                success: true,
+                message: "Please fill all the details carefully"
+            })
+        }
+
+        // check for registered user
+        let user = await User.findOne({email})  
+        // if not a registered user
+        if(!user){
+            return res.status(401).json({
+                success: true,
+                message: "User is not registered"
+            })
+        }
+
+        const payload={
+            email: user.email,
+            id: user._id,
+            role:  user.role
+        }
+        // verify password and generate a JWT token
+        if(await bcrypt.compare(password, user.password)){
+            // password matched
+            let token= jwt.sign(payload, process.env.JWT_SECRET,{
+                expiresIn: "2h"
+            })
+
+            user= user.toObject()
+            user.token= token
+            user.password= undefined
+            
+            const options = {
+                expires: new Date(Date.now() + 3 * 24 * 60 * 1000),
+                httpOnly: true
+            }
+
+            res.cookie("token",token, options).status(200).json({
+                success: true,
+                token,
+                user,
+                message: "User logged in successfully"
+            })
+        }
+        else{
+            // password do not match
+            return res.status(403).json({
+                success:false,
+                message: "Password Incorrect"
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            message: "login failure"
+        })
+    }
+}
